13fd59cbb92af4c2f1ba8e345093d8d500d6ee99
Developer01 <Developer01@DESKTOP-4ER29PC>
2024-01-11 16:33:34 +0200

    Separate the InputBox component

--- react-to-do-app/src/components/Header.jsx
+++ react-to-do-app/src/components/Header.jsx
@@ -1,9 +1,11 @@
+import InputBox from "./InputBox";
+
 export default function Header(props){
     const {title} = props;
     return(      
         <header>
             <h1>{title.toUpperCase()}</h1>
-            <input type="text" className="form-control add-todo" placeholder="Add New" />
+            <InputBox/>
         </header>
     )
 }
---
+++ react-to-do-app/src/components/InputBox.jsx
@@ -0,0 +1,5 @@
+export default function InputBox() {
+    return (
+        <input type="text" className="form-control add-todo" placeholder="Add New" />
+    );
+}
