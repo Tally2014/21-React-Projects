4bd9522ed0b1670d94a83d7ae9e434704043d9be
Developer01 <Developer01@DESKTOP-4ER29PC>
2024-01-11 15:19:52 +0200

    import bootstrap/dist/css/bootstrap.css and bootstrap/dist/css/bootstrap-theme.css

--- react-to-do-app/src/main.jsx
+++ react-to-do-app/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+// Add bootstrap
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
