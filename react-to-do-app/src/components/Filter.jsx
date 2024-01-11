849eb5bd628c1310fdba1615d763d4030e0556a6
Developer01 <Developer01@DESKTOP-4ER29PC>
2024-01-11 18:34:01 +0200

    Add a Filter component

---
+++ react-to-do-app/src/components/Filter.jsx
@@ -0,0 +1,15 @@
+export default function Filter(props) {
+  return (
+    <ul className='filters list-unstyled clearfix'>
+        <li>
+            <a className="selected">All</a>
+        </li>
+        <li>
+            <a>Active</a>
+        </li>
+        <li>
+          <a>Completed</a>
+        </li>
+    </ul>
+  )
+}
--- react-to-do-app/src/components/Footer.jsx
+++ react-to-do-app/src/components/Footer.jsx
@@ -1,11 +1,18 @@
+import Filter from './Filter';
+
 export default function Footer(props){
     const {count} = props;
     return(
-        <div className="todo-footer">
-            <strong>
-                <span className="count-todos">{count}</span>
-            </strong>
-            {' items left'}
+        <div className="todo-footer clearfix">
+            <div className='pull-left'>
+                <strong>
+                    <span className="count-todos">{count}</span>
+                </strong>
+                {' items left'}
+            </div>
+            <div className="pull-right">
+                <Filter/>
+            </div>           
         </div>
     )
 }
