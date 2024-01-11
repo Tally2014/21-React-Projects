a8736f3d4f88d63b648de0d0f2fda854514d83ea
Developer01 <Developer01@DESKTOP-4ER29PC>
2024-01-11 16:29:26 +0200

    Get the item count

--- react-to-do-app/src/components/Footer.jsx
+++ react-to-do-app/src/components/Footer.jsx
@@ -1,8 +1,9 @@
 export default function Footer(props){
+    const {count} = props;
     return(
         <div className="todo-footer">
             <strong>
-                <span className="count-todos">0</span>
+                <span className="count-todos">{count}</span>
             </strong>
             {' items left'}
         </div>
