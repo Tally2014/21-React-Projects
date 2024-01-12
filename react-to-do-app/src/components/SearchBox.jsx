aa3cab4d892bd43f44457c39cdfbc91032578ddc
Developer01 <Developer01@DESKTOP-4ER29PC>
2024-01-12 17:33:17 +0200

    Add SearchBox

---
+++ react-to-do-app/src/components/SearchBox.jsx
@@ -0,0 +1,13 @@
+export default function SearchBox(props) {
+    const {query, search} = props;
+
+    return (
+        <input
+            type="text"
+            className="form-control search"
+            value={query}
+            onChange={e => search(e.target.value)}
+            placeholder="Search"
+        />
+    );
+}
