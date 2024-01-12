67efa6408c2daf5f68b66670ebeb17574602cda2
Developer01 <Developer01@DESKTOP-4ER29PC>
2024-01-12 15:51:57 +0200

    Add an InputWrapper component

--- react-to-do-app/src/components/Header.jsx
+++ react-to-do-app/src/components/Header.jsx
@@ -1,10 +1,10 @@
-import InputBox from "./InputBox";
+import InputWrapper from './InputWrapper';
 
 export default function Header(props){
     return(      
         <header>
             <h1>Things To Do</h1>
-            <InputBox addNew={props.addNew}/>
+            <InputWrapper {...props}/>
         </header>
     )
 }
